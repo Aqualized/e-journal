c6089fb246b8f18fc483ba0085e942d14c8be365
Dmitry Shklyaev <dmitryshklyaev@outlook.com>
2024-11-22 21:27:25 +0400

    Небольшие косметические изменения

--- js/script_group_choosing.js
+++ js/script_group_choosing.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
-    // При клике на кнопку скрываем/показываем меню
-    $(".menu-toggle").on("click", function () {
-      $(".container").toggleClass("menu-closed"); // Добавляем/удаляем класс для изменения ширины
-      $(".sidebar").toggleClass("hidden"); // Скрываем/показываем боковую панель
-    });
+  $(".menu-toggle").on("click", function () {
+    $(".sidebar").toggleClass("hidden");
+    $(".container").toggleClass("menu-closed");
   });
-  
+});
